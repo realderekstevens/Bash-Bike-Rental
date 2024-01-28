893cfa93f4e5aadf7abbc50c7de2cf0009197d55
Derek <72669839+realderekstevens@users.noreply.github.com>
2024-01-27 21:08:17 -0700

    Create bike-shop.sh

---
+++ bike-shop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo -e "\n~~~~~ Bike Rental Shop ~~~~~"
+
+MAIN_MENU(){
+   if [[ $1 ]]
+   then
+      echo -e "\n$1"
+   fi
+   echo "How may I help you?"
+   echo -e "\n1. Rent a bike\n2. Return a bike \n3. Exit "
+   read MAIN_MENU_SELECTION
+   case $MAIN_MENU_SELECTION in
+   1) RENT_MENU ;;
+   2) RETURN_MENU ;;
+   3) EXIT ;;
+   *) MAIN_MENU "Please enter a valid option." ;;
+esac
+}
+
+RENT_MENU(){
+   echo "Rent Menu"
+}
+
+RETURN_MENU(){
+   echo "Return Menu"
+}
+
+EXIT(){
+   echo "Thank you for stopping in."
+}
+
+MAIN_MENU
