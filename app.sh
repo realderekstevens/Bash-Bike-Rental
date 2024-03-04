1079ac6f98a5ad1b3c63fb78f21ee313d52fa0ab
Derek <72669839+realderekstevens@users.noreply.github.com>
2024-03-03 19:31:17 -0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -13,7 +13,8 @@ MAIN_MENU(){
    fi
    echo -e "\n~~~~~ Bike Rental Shop ~~~~~"
    echo "How may I help you?"
-   echo -e "\n0. Create Database\n1. Delete Database\n2. Rent a Bike \n3. Return a Bike\n4. Exit\n5. List Databases\n6. Create Table Bikes\n7. List Tables\n"
+   echo -e "\n0. Create Database\n1. Delete Database\n2. Rent a Bike \n3. Return a Bike\n4. Exit\n5. List Databases\n6. Create Table Bikes\n7. List Tables\n8. Show Schema Table Bikes\n"
+   echo "Enter Command: "
    read MAIN_MENU_SELECTION
    case $MAIN_MENU_SELECTION in
    0) CREATE_DATABASE ;;
@@ -24,6 +25,7 @@ MAIN_MENU(){
    5) LIST_DATABASES ;;
    6) CREATE_TABLE_BIKES ;;
    7) LIST_TABLES ;;
+   8) LIST_TABLE_BIKES ;;
    *) MAIN_MENU "Please enter a valid option." ;;
 esac
 }
@@ -54,12 +56,20 @@ LIST_DATABASES(){
 
 CREATE_TABLE_BIKES(){
 	$PSQL "CREATE TABLE bikes();"
+	$PSQL "ALTER TABLE bikes ADD COLUMN bike_id SERIAL PRIMARY KEY;"
+	$PSQL "ALTER TABLE bikes ADD COLUMN type VARCHAR(50) NOT NULL;"
 	MAIN_MENU "Created Tables Bikes"
+	
 }
 
 LIST_TABLES(){
-	$PSQL "\dt"
+	$PSQL "\dt+"
 	MAIN_MENU "Listed Tables"
+}
+
+LIST_TABLE_BIKES(){
+	$PSQL "\d bikes"
+	MAIN_MENU "Listed Table Bikes"
 }
 
 MAIN_MENU
